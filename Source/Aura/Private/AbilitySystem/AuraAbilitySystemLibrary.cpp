effdf43416e7ab0f3c662f97c3e525bd43eecdf4
Enes <m.eneserdogans@gmail.com>
2024-10-02 19:43:33 +0300

    Blueprint Library

---
+++ Source/Aura/Private/AbilitySystem/AuraAbilitySystemLibrary.cpp
@@ -0,0 +1,26 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "AbilitySystem/AuraAbilitySystemLibrary.h"
+#include "UI/HUD/AuraHUD.h"
+#include "Player/AuraPlayerState.h"
+#include "UI/WidgetController/AuraWidgetController.h"
+
+UOverlayWidgetController* UAuraAbilitySystemLibrary::GetOverlayWidgetController(const UObject* WorldContextObject)
+{
+	//UGameplayStatics::GetPlayerController(WorldContextObject, 0);
+	if (APlayerController* PC = WorldContextObject->GetWorld()->GetFirstPlayerController())
+	{
+		if (AAuraHUD* AuraHUD = Cast<AAuraHUD>(PC->GetHUD()))
+		{
+			AAuraPlayerState* PS = PC->GetPlayerState<AAuraPlayerState>();
+			UAbilitySystemComponent* ASC = PS->GetAbilitySystemComponent();
+			UAttributeSet* AS = PS->GetAttributeSet();
+			const FWidgetControllerParams WidgetControllerParams(PC, PS, ASC, AS);
+			return AuraHUD->GetOverlayWidgetController(WidgetControllerParams);
+
+
+		}
+	}
+	return nullptr;
+}
