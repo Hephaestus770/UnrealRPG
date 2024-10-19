559660bf0ecdd6ac2387a8ea05e15399d33275d1
Enes <m.eneserdogans@gmail.com>
2024-10-19 12:16:42 +0300

    Upgrade to 5.4

--- Source/Aura/Public/Actor/AuraEffectActor.h
+++ Source/Aura/Public/Actor/AuraEffectActor.h
@@ -8,14 +8,14 @@
 #include "AuraEffectActor.generated.h"
 
 UENUM(BlueprintType)
-enum class EEffectApplicationPolicy
+enum class EEffectApplicationPolicy : uint8
 {
 	ApplyOnOverlap,
 	ApplyOnEndOverlap,
 	DoNotApply
 };
 UENUM(BlueprintType)
-enum class EEffectRemovalPolicy
+enum class EEffectRemovalPolicy : uint8
 {
 	RemoveOnEndOverlap,
 	DoNotRemove
