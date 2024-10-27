d4b546d162fb85106f0c65a48f1903fcd2dae915
Enes <m.eneserdogans@gmail.com>
2024-10-27 15:04:19 +0300

    ExecCalc

---
+++ Source/Aura/Private/AbilitySystem/ExecCalc/ExecCalc_Damage.cpp
@@ -0,0 +1,68 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "AbilitySystem/ExecCalc/ExecCalc_Damage.h"
+#include "AbilitySystemComponent.h"
+#include "AbilitySystem/AuraAttributeSet.h"
+
+struct AuraDamageStatics
+{
+	DECLARE_ATTRIBUTE_CAPTUREDEF(Armor);
+
+	AuraDamageStatics()
+	{
+		//Explanation of last two values: We want Target's Armor for calculations and dont want to Snapshot
+		DEFINE_ATTRIBUTE_CAPTUREDEF(UAuraAttributeSet, Armor, Target, false); 
+
+	}
+
+};
+
+static const AuraDamageStatics& DamageStatics()
+{
+	static AuraDamageStatics DStatics;
+	return DStatics;
+}
+
+
+UExecCalc_Damage::UExecCalc_Damage()
+{
+	RelevantAttributesToCapture.Add(DamageStatics().ArmorDef);
+}
+
+void UExecCalc_Damage::Execute_Implementation(const FGameplayEffectCustomExecutionParameters& ExecutionParams, 
+	FGameplayEffectCustomExecutionOutput& OutExecutionOutput) const
+{
+
+	const UAbilitySystemComponent* SourceASC = ExecutionParams.GetSourceAbilitySystemComponent();
+	const UAbilitySystemComponent* TargetASC = ExecutionParams.GetTargetAbilitySystemComponent();
+
+	AActor* SourceAvatar = nullptr;
+	AActor* TargetAvatar = nullptr;
+
+	if (SourceASC)
+	{
+		SourceAvatar = SourceASC->GetAvatarActor();
+	}
+	if (TargetASC)
+	{
+		TargetAvatar = TargetASC->GetAvatarActor();
+	}
+
+	const FGameplayEffectSpec& Spec = ExecutionParams.GetOwningSpec();
+
+	const FGameplayTagContainer* SourceTags = Spec.CapturedSourceTags.GetAggregatedTags();
+	const FGameplayTagContainer* TargetTags = Spec.CapturedTargetTags.GetAggregatedTags();
+	
+	FAggregatorEvaluateParameters EvaluationParamaters;
+	EvaluationParamaters.SourceTags = SourceTags;
+	EvaluationParamaters.TargetTags = TargetTags;
+	
+	float Armor = 0.f;
+	ExecutionParams.AttemptCalculateCapturedAttributeMagnitude(DamageStatics().ArmorDef, EvaluationParamaters, Armor);
+	Armor = FMath::Max<float>(0.f, Armor);
+	++Armor;
+
+	const FGameplayModifierEvaluatedData EvaluatedData(DamageStatics().ArmorProperty, EGameplayModOp::Additive, Armor);
+	OutExecutionOutput.AddOutputModifier(EvaluatedData);
+}
---
+++ Source/Aura/Public/AbilitySystem/ExecCalc/ExecCalc_Damage.h
@@ -0,0 +1,33 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameplayEffectExecutionCalculation.h"
+#include "ExecCalc_Damage.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class AURA_API UExecCalc_Damage : public UGameplayEffectExecutionCalculation
+{
+	GENERATED_BODY()
+	
+public:
+
+	UExecCalc_Damage();
+
+	/**
+	 * Called whenever the owning gameplay effect is executed. Allowed to do essentially whatever is desired, including generating new
+	 * modifiers to instantly execute as well.
+	 *
+	 * @note: Native subclasses should override the auto-generated Execute_Implementation function and NOT this one.
+	 *
+	 * @param ExecutionParams		Parameters for the custom execution calculation
+	 * @param OutExecutionOutput	[OUT] Output data populated by the execution detailing further behavior or results of the execution
+	 */
+	virtual void Execute_Implementation(const FGameplayEffectCustomExecutionParameters& ExecutionParams, FGameplayEffectCustomExecutionOutput& OutExecutionOutput) const override;
+
+
+};
