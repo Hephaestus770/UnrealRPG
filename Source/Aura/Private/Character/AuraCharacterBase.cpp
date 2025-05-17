1ffdb487f3ccc0b0a59ce61f6e4ade48c132c6d9
Enes <m.eneserdogans@gmail.com>
2025-05-17 05:55:01 +0300

    Gameplay Cue Notify Actor

--- Source/Aura/Private/Character/AuraCharacterBase.cpp
+++ Source/Aura/Private/Character/AuraCharacterBase.cpp
@@ -174,6 +174,11 @@ FOnDeath AAuraCharacterBase::GetOnDeathDelegate()
 	return OnDeath;
 }
 
+USkeletalMeshComponent* AAuraCharacterBase::GetWeapon_Implementation()
+{
+	return Weapon;
+}
+
 void AAuraCharacterBase::InitAbilityActorInfo()
 {
 
