2e68ffe4363cd82906a3f9d28f7ce7810cdc712e
Enes <m.eneserdogans@gmail.com>
2025-06-26 20:11:29 +0300

    Checkpoint

---
+++ Source/Aura/Private/Checkpoint/Checkpoint.cpp
@@ -0,0 +1,46 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "Checkpoint/Checkpoint.h"
+
+#include "Components/SphereComponent.h"
+
+ACheckpoint::ACheckpoint(const FObjectInitializer& ObjectInitializer)
+	: Super(ObjectInitializer)
+{
+	PrimaryActorTick.bCanEverTick = false;
+
+	CheckpointMesh = CreateDefaultSubobject<UStaticMeshComponent>("CheckPointMesh");
+	CheckpointMesh->SetupAttachment(GetRootComponent());
+	CheckpointMesh->SetCollisionEnabled(ECollisionEnabled::QueryAndPhysics);
+	CheckpointMesh->SetCollisionResponseToAllChannels(ECR_Block);
+
+	Sphere = CreateDefaultSubobject<USphereComponent>("Sphere");
+	Sphere->SetupAttachment(CheckpointMesh);
+	Sphere->SetCollisionEnabled(ECollisionEnabled::QueryOnly);
+	Sphere->SetCollisionResponseToAllChannels(ECR_Ignore);
+	Sphere->SetCollisionResponseToChannel(ECollisionChannel::ECC_Pawn, ECollisionResponse::ECR_Overlap);
+}
+
+void ACheckpoint::OnSphereOverlap(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult)
+{
+	if (OtherActor->ActorHasTag(FName("Player")))
+	{
+		HandleGlowEffects();
+	}
+}
+
+void ACheckpoint::BeginPlay()
+{
+	Super::BeginPlay();
+
+	Sphere->OnComponentBeginOverlap.AddDynamic(this, &ACheckpoint::OnSphereOverlap);
+}
+
+void ACheckpoint::HandleGlowEffects()
+{
+	Sphere->SetCollisionEnabled(ECollisionEnabled::NoCollision);
+	UMaterialInstanceDynamic* DynamicMaterialInstance = UMaterialInstanceDynamic::Create(CheckpointMesh->GetMaterial(0), this);
+	CheckpointMesh->SetMaterial(0, DynamicMaterialInstance);
+	CheckPointReached(DynamicMaterialInstance);
+}
